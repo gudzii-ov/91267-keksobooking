61797870c8782b3bb2fa81221788c3b8ee3d3a3d
Oleg Gudzii <oleg198679@gmail.com>
2018-04-22 20:25:02 +0300

    Add offers data

---
+++ js/map.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/* Массивы настроек объявлений */
+
+var AVATARS = [
+  'img/avatars/user01.png',
+  'img/avatars/user02.png',
+  'img/avatars/user03.png',
+  'img/avatars/user04.png',
+  'img/avatars/user05.png',
+  'img/avatars/user06.png',
+  'img/avatars/user07.png',
+  'img/avatars/user08.png'
+];
+
+var OFFER_TITLES = [
+  'Большая уютная квартира',
+  'Маленькая неуютная квартира',
+  'Огромный прекрасный дворец',
+  'Маленький ужасный дворец',
+  'Красивый гостевой домик',
+  'Некрасивый негостеприимный домик',
+  'Уютное бунгало далеко от моря',
+  'Неуютное бунгало по колено в воде'
+];
+
+var OFFER_TYPES = [
+  'flat',
+  'house',
+  'bungalo'
+];
+
+var OFFER_TIMES = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+
+var OFFER_FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+
+var OFFER_PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+];
