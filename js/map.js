0d1c59118bf384d4013c73bc0ecb58259c00d2b6
Oleg Gudzii <oleg198679@gmail.com>
2018-04-24 20:03:53 +0300

    Fade map

--- js/map.js
+++ js/map.js
@@ -3,7 +3,4 @@
 /* генерируем данные */
 var generatedOffers = window.dummyData.generateOffers();
 
-var mapBlock = document.querySelector('.map');
-mapBlock.classList.remove('map--faded');
-
 window.card.placePins(generatedOffers);
