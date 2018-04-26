71fbc3fa9f197927cb720547fb54c27e4247baea
Oleg Gudzii <oleg198679@gmail.com>
2018-04-26 11:25:20 +0300

    Sync type-price fields

--- js/form.js
+++ js/form.js
@@ -1,9 +1,18 @@
 'use strict';
 
 (function () {
+  var TYPE_PRICE = {
+    'bungalo': 0,
+    'flat': 1000,
+    'house': 5000,
+    'palace': 10000
+  };
+
   var formBlock = document.querySelector('.ad-form');
   var formElements = formBlock.querySelectorAll('fieldset');
   var addressField = formBlock.querySelector('#address');
+  var priceField = formBlock.querySelector('#price');
+  var typeField = formBlock.querySelector('#type');
 
   var disableFormElements = function () {
     formElements.forEach(function (element) {
@@ -32,6 +41,23 @@
     addressField.value = text;
     addressField.placeholder = text;
   };
+
+  var setPriceFieldAttributes = function (value) {
+    priceField.setAttribute('min', value);
+    priceField.setAttribute('placeholder', value);
+  };
+
+  var getTypeFieldValue = function () {
+    return typeField.options[typeField.selectedIndex].value;
+  };
+
+  var onchangeTypeHandler = function () {
+    var type = getTypeFieldValue();
+    var price = TYPE_PRICE[type];
+    setPriceFieldAttributes(price);
+  };
+
+  typeField.addEventListener('change', onchangeTypeHandler);
 
   window.form = {
     disableForm: disableForm,
