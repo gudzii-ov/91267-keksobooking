8fbc88b8e1b20f0e4d9fb10e0712397c65ae39cd
Oleg Gudzii <oleg198679@gmail.com>
2018-04-26 15:14:18 +0300

    Code refactoring

--- js/map.js
+++ js/map.js
@@ -18,42 +18,4 @@ var mainPinFirstMouseupHandler = function () {
 
 mainPinElement.addEventListener('mouseup', mainPinFirstMouseupHandler);
 
-var mainPinMousedownHandler = function (evt) {
-  evt.preventDefault();
-
-  var startCoords = {
-    x: evt.clientX,
-    y: evt.clientY
-  };
-
-  var mainPinMouseMoveHandler = function (moveEvt) {
-    moveEvt.preventDefault();
-
-    var shift = {
-      x: startCoords.x - moveEvt.clientX,
-      y: startCoords.y - moveEvt.clientY
-    };
-
-    startCoords = {
-      x: moveEvt.clientX,
-      y: moveEvt.clientY
-    };
-
-    mainPinElement.style.top = (mainPinElement.offsetTop - shift.y) + 'px';
-    mainPinElement.style.left = (mainPinElement.offsetLeft - shift.x) + 'px';
-    var mainPinActiveCoords = window.card.getPinCoords(mainPinElement, true);
-    window.form.fillAddress(mainPinActiveCoords.pinX + ', ' + mainPinActiveCoords.pinY);
-  };
-
-  var mainPinMouseUpHandler = function (upEvt) {
-    upEvt.preventDefault();
-
-    document.removeEventListener('mousemove', mainPinMouseMoveHandler);
-    document.removeEventListener('mouseup', mainPinMouseUpHandler);
-  };
-
-  document.addEventListener('mousemove', mainPinMouseMoveHandler);
-  document.addEventListener('mouseup', mainPinMouseUpHandler);
-};
-
-mainPinElement.addEventListener('mousedown', mainPinMousedownHandler);
+window.card.addMainPinMousedownHandler(mainPinElement);
